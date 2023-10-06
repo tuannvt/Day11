425b61c62aefe5a5293e89c652aa99b074bf3aef
tuannvt <tuanvancit@gmail.com>
2023-10-06 15:14:18 +0700

    add file

--- src/main/java/mvc/controller/BookController.java
+++ src/main/java/mvc/controller/BookController.java
@@ -1,2 +1,21 @@
-package mvc.controller;public class BookController {
+package mvc.controller;
+
+import java.util.List;
+import mvc.Entity.BookEntity;
+import mvc.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+  @Autowired
+  BookRepository bookRepository;
+  @RequestMapping(method = RequestMethod.GET)
+  public Object getBook(){
+    List<BookEntity>bookEntityList=bookRepository.findAll();
+    return bookEntityList;
+  }
 }
